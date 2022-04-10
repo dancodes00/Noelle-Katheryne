419ef2e52563909251d8872888fbc32cbeea6933
Seiba <kawaiiseiba23@gmail.com>
2022-04-10 11:58:10 +0800

    adding

--- index.js
+++ index.js
@@ -33,72 +33,84 @@ db.on('open', async () => {
   console.log(`Connected to noelle's data sets\nDate: ${datenow}`)
 })
 
-const atarashi = AtarashiNakama.watch()
+const atarashi = AtarashiNakama.watch([], {
+    fullDocument: `updateLookup`
+})
 
 atarashi.on("change", async next => {
     // process any change event
-  const atarashi = next.fullDocument
+    try{
+        if(next.operationType !== `insert` && next.operationType !== `update`) return 
+        if(next.operationType === `update` && !next.updateDescription.updatedFields?.isSupport?.member) return
+        const atarashi = next?.fullDocument ?? false
 
-  const user = noelle.users.cache.get(atarashi.discord.id)
-  const GUILD = noelle.guilds.cache.get('848169570954641438')
+        if(!atarashi) return
 
-  const random = Math.floor(Math.random() * 19) + 1
+        const user = noelle.users.cache.get(atarashi.discord.id)
+        const GUILD = noelle.guilds.cache.get('848169570954641438')
 
-  const canvas = Canvas.createCanvas(1100, 500)
-  const context = canvas.getContext('2d')
-  const background = await Canvas.loadImage(`./cards/${random}.jpg`)
+        const random = Math.floor(Math.random() * 19) + 1
 
-  const adjusted_width = canvas.width
-  const adjusted_height = (canvas.width / background.width) * background.height
+        const canvas = Canvas.createCanvas(1100, 500)
+        const context = canvas.getContext('2d')
+        const background = await Canvas.loadImage(`./cards/${random}.jpg`)
 
-  context.drawImage(background, 0, -Math.abs(canvas.height - adjusted_height), adjusted_width, adjusted_height)
+        const adjusted_width = canvas.width
+        const adjusted_height = (canvas.width / background.width) * background.height
 
-  const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
+        context.drawImage(background, 0, -Math.abs(canvas.height - adjusted_height), adjusted_width, adjusted_height)
 
-  const x = 30
-  const y = (canvas.height/2)-(avatar.height/2)
-  const size = Math.min(avatar.width, avatar.height)
+        const avatar = await Canvas.loadImage(user.displayAvatarURL({ format: 'png', size: 256, dynamic: true }))
 
-  context.font = applyText(canvas, user.tag)
-  context.fillStyle = 'white'
-  
-  const text_size = context.measureText(user.tag)
-  const text_height = text_size.actualBoundingBoxAscent + text_size.actualBoundingBoxDescent
-  const text_x = 25 + x + avatar.width
-  const text_y = ((canvas.height/2)-(text_height/2))+20
-  context.strokeStyle = '#38363d'
-  context.lineWidth = 5
-  context.strokeText(user.tag, text_x, text_y)
-  context.fillText(user.tag, text_x, text_y)
+        const x = 30
+        const y = (canvas.height/2)-(avatar.height/2)
+        const size = Math.min(avatar.width, avatar.height)
 
-  context.font = `20px Uni Sans Regular`
-  context.fillStyle = 'white'
-  const discordText = context.measureText(`Discord ID: ${user.id}`)
-  const discordText_height = discordText.actualBoundingBoxAscent + discordText.actualBoundingBoxDescent
-  context.strokeStyle = '#38363d'
-  context.lineWidth = 3
-  context.strokeText(`Discord ID: ${user.id}`, text_x, text_y + text_height)
-  context.fillText(`Discord ID: ${user.id}`, text_x, text_y + text_height)
-  context.strokeText(`αℓтяια Key: ${atarashi.user.id}`, text_x, text_y + text_height+discordText_height+10)
-  context.fillText(`αℓтяια Key: ${atarashi.user.id}`, text_x, text_y + text_height+discordText_height+10)
+        context.font = applyText(canvas, user.tag)
+        context.fillStyle = 'white'
+        
+        const text_size = context.measureText(user.tag)
+        const text_height = text_size.actualBoundingBoxAscent + text_size.actualBoundingBoxDescent
+        const text_x = 25 + x + avatar.width
+        const text_y = ((canvas.height/2)-(text_height/2))+20
+        context.strokeStyle = '#38363d'
+        context.lineWidth = 5
+        context.strokeText(user.tag, text_x, text_y)
+        context.fillText(user.tag, text_x, text_y)
 
-  context.beginPath()
-  context.arc(x + (avatar.width/2), y + (avatar.height/2), size * 0.5, 0, 2 * Math.PI)
-  context.closePath()
-  context.clip()
-  context.drawImage(avatar, x, y, avatar.width, avatar.height)
+        context.font = `20px Uni Sans Regular`
+        context.fillStyle = 'white'
+        const discordText = context.measureText(`Discord ID: ${user.id}`)
+        const discordText_height = discordText.actualBoundingBoxAscent + discordText.actualBoundingBoxDescent
+        context.strokeStyle = '#38363d'
+        context.lineWidth = 3
+        context.strokeText(`Discord ID: ${user.id}`, text_x, text_y + text_height)
+        context.fillText(`Discord ID: ${user.id}`, text_x, text_y + text_height)
+        context.strokeText(`αℓтяια Key: ${atarashi.user.id}`, text_x, text_y + text_height+discordText_height+10)
+        context.fillText(`αℓтяια Key: ${atarashi.user.id}`, text_x, text_y + text_height+discordText_height+10)
 
-  context.beginPath()
-  context.arc(x + (avatar.width/2), y + (avatar.height/2), size * 0.5, 0, 2 * Math.PI)
-  context.strokeStyle = 'white'
-  context.lineWidth = 10
-  context.stroke()
-  context.closePath()
+        context.beginPath()
+        context.arc(x + (avatar.width/2), y + (avatar.height/2), size * 0.5, 0, 2 * Math.PI)
+        context.closePath()
+        context.clip()
+        context.drawImage(avatar, x, y, avatar.width, avatar.height)
 
-  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `welcome-${user.username}.png`)
-  await GUILD.channels.cache.get('870747129499500595').send(`Hi ${user.toString()}, welcome to the **${GUILD.name}** server.\nWe're so glad to have you here. Be sure to check out the <#870934841263288330>, and please, Enjoy your stay.`, { files: [attachment] }).then(msg => {
-    msg.react('👋')
-  })
+        context.beginPath()
+        context.arc(x + (avatar.width/2), y + (avatar.height/2), size * 0.5, 0, 2 * Math.PI)
+        context.strokeStyle = 'white'
+        context.lineWidth = 10
+        context.stroke()
+        context.closePath()
+
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `welcome-${user.username}.png`)
+
+        const welcome_channel = next.operationType === `update` && next.updateDescription.updatedFields?.isSupport?.member ? `959999572384550932` : `870747129499500595`
+        await GUILD.channels.cache.get('870747129499500595').send(`Hi ${user.toString()}, welcome to the **${GUILD.name}** server.\nWe're so glad to have you here. Be sure to check out the <#870934841263288330>, and please, Enjoy your stay.`, { files: [attachment] }).then(msg => {
+            msg.react('👋')
+        })
+    } catch(e){
+        console.log(e)
+    }
 })
 
 noelle.once('ready', async () => {
--- schemas/atarashi.js
+++ schemas/atarashi.js
@@ -2,27 +2,31 @@ const mongoose = require('mongoose')
 const { v4: uuidv4 } = require('uuid')
 
 const atarashi = new mongoose.Schema({
-  user: {
-    id: {
-      type: String,
-      default: uuidv4()
-    }
-  },
-  discord: {
-    id: {
-      type: String,
-      required: true
+    user: {
+        id: {
+        type: String,
+        default: uuidv4
+        }
     },
-    roles: []
-  },
-  verified: {
-    type: Boolean,
-    default: true
-  },
-  banned: {
-    type: Boolean,
-    default: false
-  }
+    discord: {
+        id: {
+            type: String,
+            required: true
+        },
+        roles: []
+    },
+    verified: {
+        type: Boolean,
+        default: true
+    },
+    isMain: {
+        type: Boolean
+    },
+    isSupport: {
+        member: Boolean,
+        luka: Boolean,
+        meltryllis: Boolean
+    }
 }, { collection: 'server_members' })
 
 module.exports = mongoose.model('AtarashiNakama', atarashi)
